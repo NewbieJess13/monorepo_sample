40d16ee74580e2eb5e4603620a291b49c2a5d6ca
Jestoni Salas <tonybarbinsalas13@gmail.com>
2025-02-21 14:23:02 +0800

    feat: Added loading feature for custom button

--- packages/ui_kit/lib/src/custom_button.dart
+++ packages/ui_kit/lib/src/custom_button.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, required this.onPressed});
+  const CustomButton({super.key, required this.text, required this.onPressed,this.isLoading =false});
   final String text;
   final VoidCallback onPressed;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,9 @@ class CustomButton extends StatelessWidget {
         foregroundColor: Colors.white,
       ),
       onPressed: onPressed,
-      child: Text(text),
+      child:  isLoading ? _loadingWidget() :  Text(text),
     );
   }
+
+  Widget _loadingWidget() => CircularProgressIndicator();
 }
